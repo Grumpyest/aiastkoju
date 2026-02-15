a31867b69d4048798784a27f3ac4e7983866b40a
Andre <andre.sillari1997@gmail.com>
2026-02-15 16:28:22 +0200

    indexcss log

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,21 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
-        }
-      }
+        },
+      },
+      build: {
+        chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              react:["react", "react-dom"],
+              charts:["reacharts"],
+            },
+          },
+        },
+      },
     };
 });
